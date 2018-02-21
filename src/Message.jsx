18630b3efa9e31305ad88dd327ab8353092996c5
Konrad <konrad.kipczak@gmail.com>
2018-02-21 17:31:26 +0000

    added support for images so that they render automatically

--- src/Message.jsx
+++ src/Message.jsx
@@ -4,13 +4,21 @@ class Message extends Component {
 
   	render() {
   		if(this.props.type === "incomingMessage"){
-	    	return (
-	    		<div className="message">
-	    	  		<span className="message-username" style={{color : this.props.color}}>{this.props.user}</span>
-	    			<span className="message-content">{this.props.content}</span>
-	  	 		</div>	
-
-	    	);
+  			if(this.props.content.endsWith(".jpg") || this.props.content.endsWith(".png") || this.props.content.endsWith(".gif")){
+		    	return (
+		    		<div className="message">
+		    	  		<span className="message-username" style={{color : this.props.color}}>{this.props.user}</span>
+		    			<img src={this.props.content}/>
+		  	 		</div>	
+		    	);
+	    	} else {
+	    		return (
+		    		<div className="message">
+		    	  		<span className="message-username" style={{color : this.props.color}}>{this.props.user}</span>
+		    			<span className="message-content">{this.props.content}</span>
+		  	 		</div>	
+		    	);
+	    	}
     	} else {
     		return (
  	  	 	<div className="message system">
